d338fa31ca5c2cfa992cb97be7b3a81297f86c0e
ShubinWang <876213774@qq.com>
2017-04-15 15:12:50 +0800

    Delete page-init-state.js

--- redux/page-init-state.js
+++
@@ -1,5 +0,0 @@
-import * as UsersWangshubinWorkspacePersonalZkReactSrcPagesExamplesActionsSetState from '/Users/wangshubin/workspace/personal/zk-react/src/pages/examples/actions-set-state';
-
-export default {
-    [UsersWangshubinWorkspacePersonalZkReactSrcPagesExamplesActionsSetState.PAGE_SCOPE]: UsersWangshubinWorkspacePersonalZkReactSrcPagesExamplesActionsSetState.INIT_STATE
-};
