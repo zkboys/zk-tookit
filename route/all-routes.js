f3c78c4b58557001d2dfb6f7e5850331d3d04d4f
ShubinWang <876213774@qq.com>
2017-04-15 15:12:32 +0800

    Delete all-routes.js

--- route/all-routes.js
+++
@@ -1,9 +0,0 @@
-import UsersWangshubinWorkspacePersonalZkReactSrcPagesBaseInformationRoutes from '/Users/wangshubin/workspace/personal/zk-react/src/pages/base-information/routes';
-import UsersWangshubinWorkspacePersonalZkReactSrcPagesSystemRoutes from '/Users/wangshubin/workspace/personal/zk-react/src/pages/system/routes';
-import UsersWangshubinWorkspacePersonalZkReactSrcRoutes from '/Users/wangshubin/workspace/personal/zk-react/src/routes';
-
-export default [].concat(
-    UsersWangshubinWorkspacePersonalZkReactSrcPagesBaseInformationRoutes,
-    UsersWangshubinWorkspacePersonalZkReactSrcPagesSystemRoutes,
-    UsersWangshubinWorkspacePersonalZkReactSrcRoutes
-);
