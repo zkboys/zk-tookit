c9aa5a5f37d17be46ebc5831a93cf5ce622b0622
zkboys <876213774@qq.com>
2017-04-19 21:16:27 +0800

    合并成一个树工具

--- utils/tree-utils.js
+++ utils/tree-utils.js
@@ -1,30 +1,32 @@
 /**
- * 异步树通用方法
- * @module 异步树工具
- * */
+ * 同步数通用方法
+ * @module 同步树工具
+ */
+
+import {cloneDeep} from 'lodash/lang';
 
 /**
  * 将数据转换成tree所需格式
  * @param {object} data 要进行转换的object
  * @param {String} [keyField='id'] 指定data中某个字段转换为树所需的key
- * @param {String} [nameField='name'] 指定data中某个字段转换为树所需的name
+ * @param {String} [titleField='name'] 指定data中某个字段转换为树所需的name
  * @returns {{name: *, key: *}}
  */
-export function generateTreeNode(data, keyField = 'id', nameField = 'name') {
-    return {...data, name: data[nameField], key: data[keyField]};
+export function generateTreeNode(data, keyField = 'id', titleField = 'name') {
+    return {...data, title: data[titleField], key: data[keyField]};
 }
 
 /**
  * 将数据转换成tree所需格式
  * @param {Array} data 要进行转换的一些数据
  * @param {String} [keyField='id'] 指定data中某个字段转换为树所需的key
- * @param {String} [nameField='name'] 指定data中某个字段转换为树所需的name
+ * @param {String} [titleField='name'] 指定data中某个字段转换为树所需的name
  * @returns {Array}
  */
-export function generateTreeNodes(data, keyField = 'id', nameField = 'name') {
+export function generateTreeNodes(data, keyField = 'id', titleField = 'name') {
     let arr = [];
     if (data && data.length) {
-        arr = data.map(d => generateTreeNode(d, keyField, nameField));
+        arr = data.map(d => generateTreeNode(d, keyField, titleField));
     }
     return arr;
 }
@@ -79,17 +81,92 @@ export function appendChildrenByKey(treeData, key, child) {
 }
 
 /**
- * 根据key获取node节点
- * @param {Array} treeData 树的树状结构数据
- * @param {String} key 节点的key值
- * @returns {object} 树节点数据
+ * 检测某个节点是否有parent节点
+ * @param {array} rows 所有节点，扁平数据，非树状结构
+ * @param {object} row 需要判断得节点
+ * @returns {boolean}
  */
-export function getTreeNodeByKey(treeData, key) {
+export function hasParent(rows, row) {
+    let parentKey = row.parentKey;
+    return rows.find(r => r.key === parentKey);
+}
+
+/**
+ * js构造树方法。
+ * @param {array} rows 具有key，parentKey关系的扁平数据结构，标题字段为text
+ * @param {object} parentNode 开始节点
+ * @returns {array}
+ */
+export function convertToTree(rows, parentNode) {
+    // 这个函数会被多次调用，对rows做深拷贝，否则会产生副作用。
+    rows = cloneDeep(rows);
+    parentNode = cloneDeep(parentNode);
+
+    let nodes = [];
+    if (parentNode) {
+        nodes.push(parentNode);
+    } else { // 获取所有的顶级节点
+        nodes = rows.filter(r => !hasParent(rows, r));
+    }
+
+    // 存放要处理的节点
+    let toDo = nodes.map((v) => v);
+
+    while (toDo.length) {
+        // 处理一个，头部弹出一个。
+        let node = toDo.shift();
+        // 获取子节点。
+        rows.forEach(row => {
+            if (row.parentKey === node.key) {
+                let child = row;
+                let parentKeys = [node.key];
+                if (node.parentKeys) {
+                    parentKeys = node.parentKeys.concat(node.key);
+                }
+                child.parentKeys = parentKeys;
+
+                let parentTexts = [node.text];
+                if (node.parentTexts) {
+                    parentTexts = node.parentTexts.concat(node.text);
+                }
+                child.parentTexts = parentTexts;
+
+                let parentNodes = [{...node}];
+                if (node.parentNodes) {
+                    parentNodes = node.parentNodes.concat(parentNodes);
+                }
+                child.parentNodes = parentNodes;
+
+                if (node.children) {
+                    node.children.push(child);
+                } else {
+                    node.children = [child];
+                }
+                // child加入toDo，继续处理
+                toDo.push(child);
+            }
+        });
+    }
+
+    if (parentNode) {
+        return nodes[0].children;
+    }
+    return nodes;
+}
+
+/**
+ * 根据指定数据的键值对，查找node，比如根据path查找： getNodeByKeyValue(treeData, 'path', '/user/list')
+ * @param treeData 树状结构数据
+ * @param key key值，比如'path'，'text'等节点数据属性
+ * @param value 节点属性所对应的数据
+ * @returns {object} 返回根据 key value查找到的节点
+ */
+export function getNodeByKeyValue(treeData, key, value) {
     if (!treeData || !treeData.length) return null;
     let node = null;
     const loop = (data) => {
         for (let item of data) {
-            if (item.key === key) {
+            if (item[key] === value) {
                 node = {...item};
                 break;
             }
@@ -103,11 +180,21 @@ export function getTreeNodeByKey(treeData, key) {
 }
 
 /**
+ * 根据key查找节点
+ * @param treeData 树状结构数据
+ * @param key
+ * @returns {object} 根据key查找到的节点
+ */
+export function getNodeByKey(treeData, key) {
+    return getNodeByKeyValue(treeData, 'key', key);
+}
+
+/**
  * 根据key删除节点
  * @param {Array} treeData 树的树状结构数据
  * @param {String} key 要删除节点的key值
  */
-export function removeTreeNodeByKey(treeData, key) {
+export function removeNodeByKey(treeData, key) {
     if (!treeData || !treeData.length) return null;
     const loop = (data) => {
         for (let i = 0; i < data.length; i++) {
@@ -129,7 +216,7 @@ export function removeTreeNodeByKey(treeData, key) {
  * @param {String} key 要操作的节点的key值
  * @param {object} newNode 需要加入的子节点
  */
-export function addTreeNodeChildByKey(treeData, key, newNode) {
+export function addNodeChildByKey(treeData, key, newNode) {
     if (!treeData || !treeData.length) return null;
     newNode.isLeaf = true;
     const loop = (data) => {
@@ -155,7 +242,7 @@ export function addTreeNodeChildByKey(treeData, key, newNode) {
  * @param {Array} treeData 树的树状结构数据
  * @param {object} newNode 需要跟新的节点新数据，会根据key对原数据进行比对
  */
-export function updateTreeNode(treeData, newNode) {
+export function updateNode(treeData, newNode) {
     if (!treeData || !treeData.length) return null;
     const loop = (data) => {
         for (let item of data) {
@@ -174,11 +261,35 @@ export function updateTreeNode(treeData, newNode) {
 }
 
 /**
+ * 根据某个节点，获取其最顶级节点
+ * @param treeData 树状结构数据
+ * @param node 节点数据
+ * @returns {object} 最顶层节点
+ */
+export function getTopNodeByNode(treeData, node) {
+    if (node && !node.parentKey) return node;
+    let parentNode = null;
+    const loop = (data) => {
+        for (let item of data) {
+            if (item.key === node.parentKey) {
+                parentNode = {...item};
+                break;
+            }
+            if (item.children && item.children.length) {
+                loop(item.children);
+            }
+        }
+    };
+    loop(treeData);
+    return getTopNodeByNode(treeData, parentNode);
+}
+
+/**
  * 渲染树，cb(node[, children nodes])
  * @param {Array} treeData 树的树状结构数据
  * @param {function} cb 回调函数：cb(node[, children nodes])
  */
-export function renderTreeNode(treeData, cb) {
+export function renderNode(treeData, cb) {
     const loop = data => data.map((item) => {
         if (item.children) {
             return cb(item, loop(item.children)); // item children Item
