9af02b7952beb17f4f302f261784c71069546a03
zkboys <876213774@qq.com>
2017-06-15 10:24:11 +0800

    整理redux相关公共内容

---
+++ redux/action-utils.js
@@ -0,0 +1,26 @@
+import {createAction} from 'redux-actions';
+import * as Storage from 'zk-react/utils/storage';
+import * as types from './action-types';
+
+export default function ({pageInitState, syncKeys}) {
+    return {
+        // 同步本地数据到state中，一般在项目启动时，会调用此action进行同步。各个模块的reducer要对应的函数处理同步逻辑
+        getStateFromStorage: createAction(types.GET_STATE_FROM_STORAGE, () => {
+            let keys = syncKeys;
+            if (pageInitState) {
+                keys = keys.concat(Object.keys(pageInitState).map(key => {
+                    if (pageInitState[key] && pageInitState[key].sync === true) {
+                        return key;
+                    }
+                    return null;
+                }));
+            }
+            return Storage.multiGet(keys);
+        }, (resolved, rejected) => {
+            return {
+                resolved,
+                rejected,
+            };
+        }),
+    };
+}
