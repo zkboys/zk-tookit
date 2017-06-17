cc1a6d0cc1c94473e758773b19cf45c5dad4da3c
zkboys <876213774@qq.com>
2017-06-17 21:01:26 +0800

    去掉根目录中的index中模式判断，具体模式，具体项目中自己定义

--- index.js
+++
@@ -1,4 +0,0 @@
-export const isDev = process.env.NODE_ENV === 'development';
-export const isPro = process.env.NODE_ENV === 'production';
-export const isTest = process.env.NODE_ENV === 'test';
-export const isRC = process.env.NODE_ENV === 'rc';
