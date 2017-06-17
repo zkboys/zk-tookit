1cc8758e5361013f1e2f0c5c5c3abd186ff3bfb0
zkboys <876213774@qq.com>
2017-06-17 18:49:49 +0800

    将redux相关公共方法整理到index中，通过index引用

---
+++ redux/index.js
@@ -0,0 +1,14 @@
+import * as _actionTypes from './action-types';
+import * as _actionPage from './action-page';
+
+export const actionTypes = _actionTypes;
+export const actionPage = _actionPage;
+export actionUtils from './action-utils';
+export middlewareAsyncActionCallback from './middleware-async-action-callback';
+export checkAction from './check-action';
+export connect from './connect';
+export handleAsyncReducer from './handle-async-reducer';
+export middlewarePromise from './middleware-promise';
+export reducerPage from './reducer-page';
+export middlewareSyncReducerToLocalStorage from './middleware-sync-reducer-to-local-storage';
+export middlewareUtils from './middleware-utils';
