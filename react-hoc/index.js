941554829d76a8fd5b8526599ce1cd328599eb17
zkboys <876213774@qq.com>
2017-06-18 11:24:55 +0800

    修改包名

--- react-hoc/index.js
+++
@@ -1,3 +0,0 @@
-export domEvent from './dom-event';
-export ajax from './promise-ajax';
-export event from './event';
---
+++ react/index.js
@@ -0,0 +1,3 @@
+export domEvent from './dom-event-hoc';
+export ajax from './promise-ajax-hoc';
+export event from './event-hoc';
