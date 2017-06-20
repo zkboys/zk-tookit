ba73ac40a89ccf94d61a17cf828fc28f3046b2b1
zkboys <876213774@qq.com>
2017-06-20 15:48:22 +0800

    添加列表页组件

--- antd/index.js
+++ antd/index.js
@@ -1,3 +1,7 @@
+import * as _FormUtil from './form-util/FormUtils';
+
+export const FormUtil = _FormUtil;
+
 export EditableCell from './editable-cell/EditableCell';
 export FontIcon from './font-icon/FontIcon';
 export FontIconModal from './font-icon/FontIconModal';
@@ -5,6 +9,7 @@ export FontIconSelector from './font-icon/FontIconSelector';
 export PageContent from './page-content/PageContent';
 export PaginationComponent from './pagination/PaginationComponent';
 export QueryBar from './query-bar/QueryBar';
+export QueryItem from './query-item/QueryItem';
 export QueryResult from './query-result/QueryResult';
 export ToolBar from './tool-bar/ToolBar';
 export NoData from './no-data/NoData';
@@ -15,3 +20,4 @@ export ImageCropperModal from './image-cropper/ImageCropperModal';
 export Permission from './permission/Permission';
 export FormItemLayout from './form-item-layout/FormItemLayout';
 export InputClear from './input-clear/InputClear';
+export ListPage from './list-page/ListPage';
