5deb8d3b04bf3479e3534e70d9447579c007e10a
zkboys <876213774@qq.com>
2017-06-23 12:35:55 +0800

    可编辑单元格组件完善

--- antd/editable-cell/EditableCell.jsx
+++ antd/editable-cell/EditableCell.jsx
@@ -1,61 +1,104 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {Icon, Input, Form} from 'antd';
+import {Icon, Form} from 'antd';
+import {getFormElement} from '../form-util/FormUtils';
 import './style.less';
 
 const FormItem = Form.Item;
 
+@Form.create()
 export default class EditableCell extends Component {
+    constructor(props) {
+        super(props);
+        const currProps = this.props;
+        let showEdit;
+        if ('showEdit' in currProps) {
+            showEdit = currProps.showEdit;
+        } else if ('defaultShowEdit' in currProps) {
+            showEdit = currProps.defaultShowEdit;
+        }
+        this.state.showEdit = showEdit;
+    }
+
+    state = {
+        value: this.props.value,
+        showEdit: false,
+    };
+
     static defaultProps = {
         type: 'input', // TODO number textarea password mobile email select select-tree checkbox radio switch
         field: '',
         placeholder: '',
-        decorator: {},
         onSubmit: () => {
         },
-    }
+    };
+
     static propTypes = {
-        form: PropTypes.object.isRequired, // antd 中提供的form
+        globalForm: PropTypes.object, // antd 中提供的form
         field: PropTypes.string.isRequired,
         placeholder: PropTypes.string, // 提示
         type: PropTypes.string, // 编辑类型，默认input
+        text: PropTypes.any, // 表格显示的值 decorator.initialValue是表单元素显示的值
         decorator: PropTypes.object, // antd form的 getFieldDecorator 参数
         isRowEdit: PropTypes.bool, // 是否是整行可编辑模式
-        showEdit: PropTypes.bool, // 整行编辑模式时，编辑/非编辑形式切换
+        showEdit: PropTypes.bool, // 编辑/非编辑形式切换
+        defaultShowEdit: PropTypes.bool, // 编辑/非编辑形式切换
         onSubmit: PropTypes.func, // 单独单元格编辑时，点击'对号'，表单无校验错误时，将触发此函数，整行编辑模式，此方法无效
+        onEditChange: PropTypes.func, // 编辑/非编辑形式切换触发事件
     };
-    state = {
-        value: this.props.value,
-        editable: false,
-    }
-    edit = () => {
-        this.setState({editable: true});
-        setTimeout(() => {
-            this.input.focus();
-            this.input.refs.input.select();
-        }, 0);
-    }
-    handleSubmit = () => {
-        const {form, onSubmit, field} = this.props;
-        form.validateFieldsAndScroll((err, values) => {
-            if (!err) {
-                this.setState({editable: false});
-                onSubmit(values[field]);
-            }
-        });
+
+    componentWillReceiveProps(nextProps) {
+        // 如果 props 传入 showEdit,则直接更新
+        if ('showEdit' in nextProps) {
+            this.setState({
+                showEdit: nextProps.showEdit,
+            });
+        }
     }
 
+    edit = () => {
+        this.toggleEdit(true);
+    };
+
+    toggleEdit = (showEdit) => {
+        const {onEditChange} = this.props;
+        // props 中存在 defaultActiveIndex 时,则更新
+        if ('defaultShowEdit' in this.props) {
+            this.setState({
+                showEdit,
+            });
+            // 更新后执行回调函数
+            if (onEditChange) onEditChange(showEdit);
+        }
+    };
+
+    handleSubmit = () => {
+        const {onSubmit} = this.props;
+        const form = 'globalForm' in this.props ? this.props.globalForm : this.props.form;
+        form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                onSubmit(values);
+                this.toggleEdit(false);
+            }
+        });
+    };
+
     render() {
-        const {editable} = this.state;
-        let {isRowEdit, showEdit, form: {getFieldDecorator}, field, decorator, placeholder} = this.props;
+        const {showEdit} = this.state;
+        let {
+            isRowEdit,
+            field,
+            text,
+            decorator = {},
+        } = this.props;
+        const form = 'globalForm' in this.props ? this.props.globalForm : this.props.form;
+        let {getFieldDecorator} = form;
+
+        if (!decorator.initialValue) decorator.initialValue = text;
         let showIcon = true;
         if (isRowEdit) {
             showIcon = false;
-        } else {
-            showEdit = editable;
         }
-
-        const value = decorator.initialValue;
         return (
             <div className="editable-cell">
                 {
@@ -64,7 +107,7 @@ export default class EditableCell extends Component {
                             <Form onSubmit={this.handleSubmit}>
                                 <FormItem>
                                     {getFieldDecorator(field, decorator)(
-                                        <Input ref={node => this.input = node} placeholder={placeholder}/>
+                                        getFormElement(this.props, form)
                                     )}
                                 </FormItem>
                             </Form>
@@ -81,7 +124,7 @@ export default class EditableCell extends Component {
                         </div>
                         :
                         <div className="editable-cell-text-wrapper">
-                            {value || ' '}
+                            {text || ' '}
                             {
                                 showIcon ?
                                     <Icon
