a24ff230a8c15474ae2ae39c10fad136aedf8588
zkboys <876213774@qq.com>
2017-07-27 15:35:23 +0800

    tool item 默认8px间隔

--- antd/tool-item/ToolItem.jsx
+++ antd/tool-item/ToolItem.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Button} from 'antd';
 import {FontIcon} from 'zk-tookit/antd';
-
+import './style.less';
 
 export default class ToolItem extends Component {
     static defaultProps = {
@@ -28,6 +28,7 @@ export default class ToolItem extends Component {
                 {
                     items.map((item, index) => {
                         const {
+                            key,
                             type = 'primary',
                             icon,
                             text,
@@ -37,11 +38,17 @@ export default class ToolItem extends Component {
                             onClick = () => {
                             },
                         } = item;
+                        const itemKey = key || index;
                         if (permission && !hasPermission(permission)) return null;
-                        if (getComponent) return <span key={index}>{getComponent()}</span>;
-                        if (component) return <span key={index}>{component}</span>;
+                        if (getComponent) return <span className="zk-tookit-tool-item" key={itemKey}>{getComponent()}</span>;
+                        if (component) return <span className="zk-tookit-tool-item" key={itemKey}>{component}</span>;
                         return (
-                            <Button key={index} type={type} onClick={onClick}>
+                            <Button
+                                className="zk-tookit-tool-item"
+                                key={itemKey}
+                                type={type}
+                                onClick={onClick}
+                            >
                                 {
                                     icon ?
                                         <FontIcon type={icon} style={{marginRight: 8}}/>
