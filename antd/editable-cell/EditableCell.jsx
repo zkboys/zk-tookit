f6f62e989960a4dcb70ea69e414edd382c33a67c
zkboys <876213774@qq.com>
2017-08-30 14:58:28 +0800

    重构

--- antd/editable-cell/EditableCell.jsx
+++ antd/editable-cell/EditableCell.jsx
@@ -6,10 +6,9 @@ import {getFormElement} from '../form-util/form-utils';
 import './style.less';
 
 /**
- * 可编辑单元格封装，全局form使用 EditableCell，可以显示错误提示
- * 每个 EditableCell 有自己的from，使用 export default Form.create()(EditableCell);
+ * 可编辑单元格封装
  */
-export class EditableRowCell extends Component {
+class EditableCell extends Component {
     constructor(props) {
         super(props);
         const currProps = this.props;
@@ -20,10 +19,11 @@ export class EditableRowCell extends Component {
             showEdit = currProps.defaultShowEdit;
         }
         this.state.showEdit = showEdit;
+
+        this.form = this.props.outerForm || this.props.form;
     }
 
     state = {
-        value: this.props.value,
         showEdit: false,
     };
 
@@ -36,17 +36,19 @@ export class EditableRowCell extends Component {
     };
 
     static propTypes = {
-        globalForm: PropTypes.object, // antd 中提供的form fixme: 改名为outerForm，所有form相关的组件都可这样封装，外部传入from可以提高灵活性；内部form可以简化组件。
-        field: PropTypes.string.isRequired,
-        placeholder: PropTypes.string, // 提示
-        type: PropTypes.string, // 编辑类型，默认input
+        outerForm: PropTypes.object, // antd 中提供的form
         text: PropTypes.any, // 表格显示的值 decorator.initialValue是表单元素显示的值
-        decorator: PropTypes.object, // antd form的 getFieldDecorator 参数
         isRowEdit: PropTypes.bool, // 是否是整行可编辑模式
         showEdit: PropTypes.bool, // 编辑/非编辑形式切换
         defaultShowEdit: PropTypes.bool, // 编辑/非编辑形式切换
         onSubmit: PropTypes.func, // 单独单元格编辑时，点击'对号'，表单无校验错误时，将触发此函数，整行编辑模式，此方法无效
         onEditChange: PropTypes.func, // 编辑/非编辑形式切换触发事件
+
+        field: PropTypes.string,
+        placeholder: PropTypes.string, // 提示
+        type: PropTypes.string, // 编辑类型，默认input
+        decorator: PropTypes.object, // antd form的 getFieldDecorator 参数
+        elementProps: PropTypes.object, // 表单元素props
     };
 
     componentWillReceiveProps(nextProps) {
@@ -64,22 +66,13 @@ export class EditableRowCell extends Component {
 
     toggleEdit = (showEdit) => {
         const {onEditChange} = this.props;
-        // props 中存在 defaultActiveIndex 时,则更新
-        if ('defaultShowEdit' in this.props) {
-            this.setState({
-                showEdit,
-            });
-            // 更新后执行回调函数
-            if (onEditChange) onEditChange(showEdit);
-        } else {
-            this.setState({showEdit});
-        }
+        this.setState({showEdit});
+        if (onEditChange) onEditChange(showEdit);
     };
 
     handleSubmit = () => {
         const {onSubmit} = this.props;
-        const form = 'globalForm' in this.props ? this.props.globalForm : this.props.form;
-        form.validateFieldsAndScroll((err, values) => {
+        this.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
                 onSubmit(values);
                 this.toggleEdit(false);
@@ -95,14 +88,12 @@ export class EditableRowCell extends Component {
             text,
             decorator = {},
         } = this.props;
-        const form = 'globalForm' in this.props ? this.props.globalForm : this.props.form;
-        let {getFieldDecorator} = form;
+        let {getFieldDecorator} = this.form;
 
         if (!decorator.initialValue) decorator.initialValue = text;
-        let showIcon = true;
-        if (isRowEdit) {
-            showIcon = false;
-        }
+
+        let showIcon = !isRowEdit;
+
         return (
             <div className="editable-cell">
                 {
@@ -111,7 +102,7 @@ export class EditableRowCell extends Component {
                             <Form onSubmit={this.handleSubmit}>
                                 <FormItemLayout colon={false} labelWidth={22} label=" ">
                                     {getFieldDecorator(field, decorator)(
-                                        getFormElement(this.props, form)
+                                        getFormElement(this.props, this.form)
                                     )}
                                 </FormItemLayout>
                             </Form>
@@ -145,6 +136,5 @@ export class EditableRowCell extends Component {
     }
 }
 
-const EditableCell = Form.create()(EditableRowCell);
-export default EditableCell;
+export default Form.create()(EditableCell);
 
